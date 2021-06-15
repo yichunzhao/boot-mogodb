19e837c64ebe06ea510be510537b771027726dde
yichunzhao <zhaoyichun@hotmail.com>
2021-06-15 18:39:33 +0200

    Initial commit

---
+++ src/main/java/com/ynz/demo/bootmogodb/BootMogodbApplication.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.bootmogodb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BootMogodbApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BootMogodbApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ynz/demo/bootmogodb/BootMogodbApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ynz.demo.bootmogodb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BootMogodbApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
