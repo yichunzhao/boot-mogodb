7714b226a9a6b6f93dd23e9a0d870f814889ec9b
yichunzhao <zhaoyichun@hotmail.com>
2021-06-16 22:59:11 +0200

    using Spring MongoDB core criteria Query to query a document

--- src/main/java/com/ynz/demo/bootmongodb/BootMongodbApplication.java
+++ src/main/java/com/ynz/demo/bootmongodb/BootMongodbApplication.java
@@ -4,16 +4,21 @@ import com.ynz.demo.bootmongodb.dao.StudentRepository;
 import com.ynz.demo.bootmongodb.domain.Address;
 import com.ynz.demo.bootmongodb.domain.Gender;
 import com.ynz.demo.bootmongodb.domain.Student;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
 import java.util.List;
 
 @SpringBootApplication
+@Slf4j
 public class BootMongodbApplication {
 
     public static void main(String[] args) {
@@ -21,18 +26,32 @@ public class BootMongodbApplication {
     }
 
     @Bean
-    CommandLineRunner commandLineRunner(StudentRepository repository) {
+    CommandLineRunner commandLineRunner(StudentRepository repository, MongoTemplate mongoTemplate) {
         return (args) -> {
             Address address = Address.builder().city("Cph").country("DK").postCode("1000").build();
 
-            Student student = Student.builder().firstName("Yichun").lastName("Zhao").email("ynz@hotmail.com")
+            String email = "ynz@hotmail.com";
+
+            Student student = Student.builder().firstName("Yichun").lastName("Zhao").email(email)
                     .gender(Gender.MALE).favoriteSubjects(List.of("Food", "Hiking", "Coding", "History"))
                     .totalSpentInBooks(BigDecimal.valueOf(5))
                     .address(address).createdAt(LocalDateTime.now())
                     .build();
 
-            repository.insert(student);
+            //from mongo core
+            Query query = new Query();
 
+            //creating search Criteria
+            query.addCriteria(Criteria.where("email").is(email));
+
+            List<Student> students = mongoTemplate.find(query, Student.class);
+
+            if (students.size() >= 1) {
+                throw new IllegalStateException("The email has been used");
+            }
+
+            log.info("inserting student" + student.toString());
+            repository.insert(student);
         };
     }
 
