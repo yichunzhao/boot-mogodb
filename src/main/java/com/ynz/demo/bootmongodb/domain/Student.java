da0d21e841c949d5bde1b3f3afb8be64aca74baa
yichunzhao <zhaoyichun@hotmail.com>
2021-06-16 22:28:32 +0200

    index a documentation by a field(uniqueness), and enabling auto-index-generation

--- src/main/java/com/ynz/demo/bootmongodb/domain/Student.java
+++ src/main/java/com/ynz/demo/bootmongodb/domain/Student.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.math.BigDecimal;
@@ -19,6 +20,7 @@ public class Student {
     private String id;
     private String firstName;
     private String lastName;
+    @Indexed(unique = true)
     private String email;
     private Gender gender;
     private Address address;
